22ba3f7a40f4c0009005e53f5b8a528a13379f6f
laupierre <laupierre@users.noreply.github.com>
2023-07-11 14:13:11 +0200

    Update gprofiler_ora.R

--- gprofiler_ora.R
+++ gprofiler_ora.R
@@ -1,4 +1,6 @@
 library (gprofiler2)
+library (org.Mm.eg.db)
+
 
 deg <- readRDS ("deg.rds")
 
@@ -11,7 +13,6 @@ genes <- gsub ("\\..*", "", genes)
 
 geneUniverse <- as.character (deg$Geneid)
 geneUniverse  <- gsub ("\\..*", "", geneUniverse)
-
 
 # GO:BP vs GO
 gp_up <- gost(query = genes, organism = "mmusculus", numeric_ns = "ENTREZGENE_ACC", sources = c("GO"),
@@ -45,6 +46,46 @@ head (gsedf)
 write.table (gsedf, "GOA_gprofiler_increased_genes.txt", sep="\t", quote=F, row.names=F)
 
 
+
+#### Negative genes
+
+deg.neg <- na.omit (deg[deg$log2FoldChange <0 & deg$padj < 0.05, ])
+genes <- as.character (deg.neg$Geneid)
+genes <- gsub ("\\..*", "", genes)
+
+geneUniverse <- as.character (deg$Geneid)
+geneUniverse  <- gsub ("\\..*", "", geneUniverse)
+
+# GO:BP vs GO
+gp_up <- gost(query = genes, organism = "mmusculus", numeric_ns = "ENTREZGENE_ACC", sources = c("GO"),
+             exclude_iea=TRUE, evcodes = TRUE, significant = FALSE, correction_method = "fdr", custom_bg= geneUniverse)
+
+gp <- gp_up$result[ ,c("source", "term_id", "term_name", "p_value", "significant", "intersection_size", "intersection")]
+colnames (gp)[colnames (gp) == "p_value"] <- "padj"
+gp <- gp[order (gp$padj), ]
+gp <- gp[gp$padj < 0.05, ]
+head(gp)
+
+
+# Adding back gene symbols
+
+res <- list ()
+
+for (i in (1:dim (gp)[1])) {
+print (i)
+entrez <- unlist (strsplit (as.vector (gp$intersection [i]), split=","))
+symbols <- AnnotationDbi::select(org.Mm.eg.db, keys= entrez, keytype="ENSEMBL", columns="SYMBOL")
+res[[i]] <- unique (symbols$SYMBOL)
+}
+
+res <- lapply (res, function (x) {paste (x,collapse="," )} )
+x <- t (data.frame (res))
+row.names (x) <- 1:dim (gp)[1]
+gsedf <- cbind (data.frame (gp), genes= x)
+gsedf <- gsedf[ ,!colnames (gsedf) %in% c("intersection")]
+head (gsedf)
+
+write.table (gsedf, "GOA_gprofiler_decreased_genes.txt", sep="\t", quote=F, row.names=F)
 
 
 
